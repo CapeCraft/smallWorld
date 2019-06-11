2c6807e7d763517d6e63d2937f099e46c9acc99d
mov51 <mov51@mov51.net>
2019-06-11 07:07:01 -0400

    Created getRegion and getChunk methods Used above methods to clean up code moved region file listing to its own method

--- src/net/mov51/Main.java
+++ src/net/mov51/Main.java
@@ -9,28 +9,37 @@ public class Main {
     }
     public static void listRegionsByRadius(int x, int z, int radius ){
         int corner1X, corner2X, corner1Z, corner2Z; // initializing corner variables
-        corner1X = corner2X = x - radius; // setting x coordinate corner variables to account for radius
-        corner1Z = corner2Z = z + radius; // setting z coordinate corner variables to account for radius
+        corner1X = corner1Z = x - radius; // setting x coordinate corner variables to account for radius
+        corner2Z = corner2X = z + radius; // setting z coordinate corner variables to account for radius
 
-        int corner1ChunkX = corner1X / 16; // getting chunk coordinates for the 2 corner positions TODO create a method that returns each coordinates chunk position
-        int corner1ChunkZ = corner1Z / 16;
-        int corner2ChunkX = corner2X / 16;
-        int corner2ChunkZ = corner2Z / 16;
 
-        int corner1RegionX = (int)Math.floor(corner1ChunkX / 32.0); // finding region positions for the 2 corners TODO create a method that returns each coordinates region position
-        int corner1RegionZ = (int)Math.floor(corner1ChunkZ / 32.0);
-        int corner2RegionX = (int)Math.floor(corner2ChunkX / 32.0);
-        int corner2RegionZ = (int)Math.floor(corner2ChunkZ / 32.0);
-//        TODO move region listing to "listRegionsByRectangle" and make radius processing cleaner.
-        for(int i = corner1RegionZ; i<=corner2RegionZ; i++) { // looping for the length between corner 1 and 2 z
-            for(int j = corner1RegionX; j<=corner2RegionX; j++){ // looping for the length between corner 1 and 2 x
-                System.out.println("r." + i + "." + j + ".mca"); // printing formatted region file names TODO will need to change output in a way to be processed by another method
-            }
-        }
+        int corner1RegionX = getRegion(getChuck(corner1X)); // finding region positions for the 2 corners TODO create a method that returns each coordinates region position
+        int corner1RegionZ = getRegion(getChuck(corner1Z)); // calling the get chunk method to return the chunk coordinates required for the getRegion method
+        int corner2RegionX = getRegion(getChuck(corner2X));
+        int corner2RegionZ = getRegion(getChuck(corner2Z));
+
+        // calling the listRegionsByRectangle method after turning the radius and point coordinate into a rectangle
+        listRegionsByRectangle(corner1RegionX, corner1RegionZ, corner2RegionX,corner2RegionZ );
+
 
     }
     public static void listRegionsByRectangle(int x1, int z1, int x2, int z2){
-//        TODO create a method that lists regions from 2 coordinate positions, called by other methods after processing their form of data I.E. radius or a predefined area.
+        // takes a predefined rectangle and lists all regions within that area
+        for(int i = z1; i<=z2; i++) { // looping for the length between corner 1 and 2 z
+            for(int j = x1; j<=x2; j++){ // looping for the length between corner 1 and 2 x
+                System.out.println("r." + i + "." + j + ".mca"); // printing formatted region file names TODO will need to change output in a way to be processed by another method
+            }
+        }
+        // TODO send output in a way that it can be looped through to copy files from a functioning Minecraft world
+    }
+
+    public static int getChuck(int inputCord){
+        return inputCord / 16;
+        // takes input block coordinates and returns the chunk coordinates
+    }
+    public static int getRegion(int inputChunk){
+        return (int)Math.floor(inputChunk / 32.0);
+        // takes input chunk coordinates and returns the region file coordinates
     }
 }
 
